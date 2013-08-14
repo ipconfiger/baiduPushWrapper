46489d543d0b85060389d4bc2eda3ca025f53be3
ipconfiger <superpowerlee@gmail.com>
2013-08-14 22:34:23 +0800

    发布到pypi

---
+++ setup.py
@@ -0,0 +1,17 @@
+#coding=utf8
+from setuptools import setup
+
+__author__ = 'Alexander.Li'
+
+setup(
+    name='baiduPushWrapper',
+    version='0.0.0.1 pre',
+    packages=['baiduPushWrapper'],
+    author='Alexander.Li',
+    author_email='superpowerlee@gmail.com',
+    license='LGPL',
+    install_requires=["requests>=1.2.0",],
+    description="Wrapper of baidu push api",
+    keywords ='baidu push',
+    url="https://github.com/ipconfiger/baiduPushWrapper"
+)
