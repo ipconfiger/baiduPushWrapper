3936969e14ac8db4af53a6a35727d41904ea460f
ipconfiger <superpowerlee@gmail.com>
2013-08-14 23:30:43 +0800

    修正一个bug

--- baiduPushWrapper/connExceptions.py
+++ baiduPushWrapper/connExceptions.py
@@ -1,9 +1,13 @@
 #coding=utf8
 __author__ = 'Alexander.Li'
 
-class AccessError(SystemError):
+class AccessError(Exception):
     """
     错误类，用于区分其他底层异常
     """
-    def __init__(self, *argv, **kwargs):
-        super(AccessError).__init__(*argv, **kwargs)
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
